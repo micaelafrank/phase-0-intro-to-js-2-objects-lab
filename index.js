e4b4f3dd13c639d9f6b06b880b5fe5657c63a9d2
Micaela Frank <micaelalfrank@gmail.com>
2022-04-26 21:20:36 -0400

    commit tree 985abea5581e874e688431a9ad9aa43d23e79e59

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee ={
+    name: "Micaela",
+    streetAddress: "193 7th Avenue",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployeeKeyValue = {...employee, [key]: value,};
+    return newEmployeeKeyValue;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
